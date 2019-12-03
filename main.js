acab9c519e5fad3d3ebb7bc8f29e0976261fc6b3
Maran Filippo <filippo.maran.fm91@gmail.com>
2019-12-03 13:03:34 +0100

    aggiunto cambio icona da microfono ad aereo, aggiunto invio messaggio anche con invio..yahooo

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-var messaggio = $(".messaggio.template").clone();
-messaggio.removeClass("template").addClass("inviato");
 $(".vocale-invia i").click(function(){
+    invia_messaggio();
+})
+$("#invia-messaggio").keypress(function(event){
+    if(event.which == 13){
+        invia_messaggio();
+    }
+})
+function invia_messaggio(){
     var messaggioUtente = $("#invia-messaggio").val();
-    messaggio.children(".testo-messaggio").text(messaggioUtente);
-    $(".main-messaggi").append(messaggio);
+    if (messaggioUtente.length != 0) {
+        var messaggio = $(".messaggio.template").clone();
+        messaggio.children(".testo-messaggio").text(messaggioUtente);
+        messaggio.removeClass("template").addClass("inviato");
+        $(".conversazione.active").append(messaggio);
+        $("#invia-messaggio").val("");
+    }
+}
+$("#invia-messaggio").keyup(function(event){
+    var messaggioUtente = $("#invia-messaggio").val();
+    if (messaggioUtente.length != 0) {
+        $(".vocale-invia i").removeClass("fa fa-microphone").addClass("fas fa-paper-plane")
+    } else {
+        $(".vocale-invia i").addClass("fa fa-microphone").removeClass("fas fa-paper-plane")
+    }
 })
